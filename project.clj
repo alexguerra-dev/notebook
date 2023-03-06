abffc4ab57d2c10124fc976190034775cc6e71ef
Alex Guerra <alexander.a.guerra@gmail.com>
2023-03-06 09:07:54 -0600

    Add lein project files

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject notebook "0.1.0-SNAPSHOT"
+  :description "A place for my thoughts. It's in Clojure!"
+  :url "https://github.com/alexguerra-dev/notebook"
+  :license {:name "MIT"
+            :url "https://choosealicense.com/licenses/mit"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot notebook.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
