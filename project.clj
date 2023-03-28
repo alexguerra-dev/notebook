30a27d3b8771ae0f1468a1dd84b1179267e86f53
Alex Guerra <alexander.a.guerra@gmail.com>
2023-03-27 20:29:35 -0500

    Update project file

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A place for my thoughts. It's in Clojure!"
   :url "https://github.com/alexguerra-dev/notebook"
   :license {:name "MIT"
-            :url "https://choosealicense.com/licenses/mit"}
+            :url "https://mit-license.org"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot notebook.core
   :target-path "target/%s"
